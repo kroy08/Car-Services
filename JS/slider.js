097a8424a53ea206bd71199e21024fb4e77c345b
kroy08 <kevinroy259@gmail.com>
2024-07-05 15:42:04 +0800

    update

--- JS/slider.js
+++ JS/slider.js
@@ -1,11 +1,25 @@
 var slider = tns({
-    container: ".testimonial__slider",
-    items: 3,
+    container: ".services__sliders",
+    items: 1,
     speed: 200,
     gutter: 100,
     slideBy: "page",
     autoplay: true,
     navPosition: "bottom",
+    controls: false,
+  
+});
+
+var slider = tns({
+    container: ".services__cards",
+    items: 3,
+    speed: 200,
+    gutter: 100,
+    slideBy: "page",
+    nav: false,
+    autoplay: true,
+    navPosition: "bottom",
+    controls: true,
     containerControls: "#controls",
     prevButton: ".prev",
     nextButton: ".next",
@@ -16,6 +30,5 @@ var slider = tns({
         200: {
             items:1,
         },
-    },
+    },  
 });
-
